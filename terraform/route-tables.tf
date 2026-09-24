d51b1be800e3e8f02e557bbe8a2ab753c0c9ee18
hemant20255-cloud <hemant20255@gmail.com>
2026-09-24 16:16:31 +0530

    Add VPC routing and NAT gateway

---
+++ terraform/route-tables.tf
@@ -0,0 +1,56 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name        = "${var.project_name}-public-rt"
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    tier        = "public"
+  }
+}
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name        = "${var.project_name}-nat-eip"
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+  depends_on    = [aws_internet_gateway.main]
+  tags = {
+    Name        = "${var.project_name}-nat"
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+  tags = {
+    Name        = "${var.project_name}-private-rt"
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    tier        = "private"
+  }
+}
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
